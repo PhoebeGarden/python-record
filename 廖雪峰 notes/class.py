1e4689f817239155df5e235d849ccb1c16b3e33b
Phoebe <liuyu1768@163.com>
2017-06-04 22:43:48 +0800

    class.py
    类的基本概念，数据封装、继承和多态

--- 廖雪峰 notes/class.py
+++ 廖雪峰 notes/class.py
@@ -26,3 +26,49 @@ print(bart._Student__name)
 print(bart.__name)
 
 #__name会被某一些解释器改成_Student__name，但这种做法不建议，其他python解释器可能不是这种做法
+
+import types
+print(type(123))
+print(type('123'))
+print(type(None))
+print(type(bart))
+def fn():
+	pass
+print(type(fn))
+print(type(abs))
+print(type(lambda x:x))
+print(type((x for x in range(10))))
+
+print(isinstance('123', str))
+print(isinstance(b'a', bytes))
+print(isinstance((1, 2, 3), (list, tuple)))
+
+print(dir('ABC'))
+
+print(len('ABC'))
+print('ABC'.__len__())
+
+class MyObject(object):
+	def __init__(self):
+		self.x=9
+	def power(self):
+		return self.x*self.x
+obj = MyObject()
+
+print(hasattr(obj,'x'))
+print(hasattr(obj,'y'))
+setattr(obj, 'y', 19)
+print(hasattr(obj,'y'))
+print(getattr(obj,'y'))
+print(getattr(obj,'z', 404))
+
+class Student(object):
+	"""docstring for Student"""
+	def __init__(self, name):
+		self.name = name
+	name = 'Student'
+s = Student('Bob')
+s.score = 90
+print(s.name)
+del s.name
+print(s.name)
